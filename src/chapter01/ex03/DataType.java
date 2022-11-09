bafb7b4a03b412dd9a970fcb516eb2c231a9ad86
Hayeon Lee <formahappy@gmail.com>
2022-11-09 14:57:29 +0900

    11월 9일 커밋 내용

---
+++ src/chapter01/ex03/DataType.java
@@ -0,0 +1,26 @@
+package chapter01.ex03;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// 변수를 생성후 출력하기 
+		// 변수를 선언 전에 변수에 어떤 데이터 형식을 저장할지 선언하는 것.
+		int a ; // 변수 선언, int  = 정수를 담는 변수
+		a = 10;
+		
+		System.out.println(a);
+		a = 20;
+		System.out.println(a);
+	
+		// a = 10.5; // int 변수는 실수를 담을 수 없다.
+	
+		float b ; //float : 실수를 저장할 수 있는 변수 
+		b = 10;		// 정수가 실수로 변환 
+		System.out.println(b);
+		
+		double c ; 
+		c = 10.5;
+		System.out.println(c);
+	}
+
+}
