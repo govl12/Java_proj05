27fc9aad452cb76c0b5684b997e1cc7709fc7cfb
Hayeon Lee <formahappy@gmail.com>
2022-11-09 11:37:58 +0900

    첫번째 커밋

---
+++ src/chapter01/ex01/Test_Class.java
@@ -0,0 +1,15 @@
+package chapter01.ex01;
+
+public class Test_Class {
+	public static void main(String[] args) {
+	
+		// 패키지 생성, 클래스 생성, 클래스를 패키지 사이로 이동, 하위패키지 생성
+		// 기본적으로 JSP나 Spring을 구성 할 때 3단계의 package가 기본적으로 등록
+		// com.naver.www : 클래스 저장 
+	
+		
+		
+	}
+}
+	
+
